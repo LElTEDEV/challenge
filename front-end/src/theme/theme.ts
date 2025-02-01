99bbdd6a132c49ec2531b16079d04d27ce5fe254
igor <suporteaser2@gmail.com>
2025-02-01 13:50:36 -0300

    iniciando o front-end

---
+++ front-end/src/theme/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  COLORS: {
+    gray_100: "#f8f8ff",
+    gray_200: "#eeeeee",
+
+    gray_500: "#ecedf6",
+
+    gray_800: "#cccddf",
+
+    white: "#ffffff",
+
+    blue_500: "#6370f0",
+  },
+};
